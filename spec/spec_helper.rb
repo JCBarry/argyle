5755782ebdd71165f000821d94bb59eb0216faac
Jason Barry <jay@jcbarry.com>
2015-12-10 14:54:41 -0500

    reconfigure rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,12 +5,21 @@ require 'rspec/rails'
 
 Rails.backtrace_cleaner.remove_silencers!
 
-# Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'argyle'
+
 RSpec.configure do |config|
-  config.mock_with :rspec
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
-  config.order = "random"
   config.infer_spec_type_from_file_location!
 end
