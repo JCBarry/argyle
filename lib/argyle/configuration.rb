6770077454500990927e272bf04f490277cf9284
Jason Barry <jay@jcbarry.com>
2015-12-17 15:28:59 -0500

    add plaid config to argyle config

--- lib/argyle/configuration.rb
+++ lib/argyle/configuration.rb
@@ -1,12 +1,17 @@
 module Argyle
   class Configuration
-    attr_accessor :plaid_src, :product, :key, :env, :long_tail
+    attr_accessor :plaid_src, :product, :key, :env, :long_tail, :customer_id, :secret
 
     def initialize
       @plaid_src = "https://cdn.plaid.com/link/stable/link-initialize.js"
-      @product = 'connect'
+      @product = 'auth'
       @env = 'tartan'
       @long_tail = false
+    end
+
+    def environment_location
+      return "https://api.plaid.com/" if @env == 'production'
+      "https://tartan.plaid.com/"
     end
   end
 end
--- spec/argyle/configuration_spec.rb
+++ spec/argyle/configuration_spec.rb
@@ -7,19 +7,27 @@ describe Argyle::Configuration do
     it 'should have a default value' do
       expect(Argyle::Configuration.new.plaid_src).to eq("https://cdn.plaid.com/link/stable/link-initialize.js")
     end
+
+
+    it 'can set value' do
+      config = Argyle::Configuration.new
+      config.plaid_src = "http://example.com"
+
+      expect(config.plaid_src).to eq("http://example.com")
+    end
   end
 
   describe '#product=' do
     it 'has a default value' do
       config = Argyle::Configuration.new
-      expect(config.product).to eq("connect")
+      expect(config.product).to eq("auth")
     end
 
     it 'can set value' do
       config = Argyle::Configuration.new
-      config.product = "auth"
+      config.product = "connect"
 
-      expect(config.product).to eq("auth")
+      expect(config.product).to eq("connect")
     end
   end
 
@@ -52,4 +60,35 @@ describe Argyle::Configuration do
     end
   end
 
+
+  describe '#customer_id=' do
+    it 'can set value' do
+      config = Argyle::Configuration.new
+      config.customer_id = 'myId'
+
+      expect(config.customer_id).to eq('myId')
+    end
+  end
+
+  describe '#secret=' do
+    it 'can set value' do
+      config = Argyle::Configuration.new
+      config.secret = 'mysecret'
+
+      expect(config.secret).to eq('mysecret')
+    end
+  end
+
+  describe '#environment_location' do
+    it 'has a default value (test)' do
+      config = Argyle::Configuration.new
+      expect(config.environment_location).to eq('https://tartan.plaid.com/')
+    end
+
+    it 'has a production value' do
+      config = Argyle::Configuration.new
+      config.env = 'production'
+      expect(config.environment_location).to eq('https://api.plaid.com/')
+    end
+  end
 end
